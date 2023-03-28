6d0324485bcc63ba86494d9c08330df4c5604d25
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-03-28 15:25:10 -0500

    Se creo navbar con su respectiva funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/experience' element={<Experience/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
