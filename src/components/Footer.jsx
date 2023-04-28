ec0e7b49dd567d41fbba76ce6ffe0f402ba99174
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-04-28 15:40:34 -0500

    Icono de email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { MdFacebook } from "react-icons/md";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
+import {GrMail} from 'react-icons/gr'
+import { FaLinkedinIn, FaGithub, FaYoutube } from "react-icons/fa";
 import '../styles/Footer.css'
 
 export default function Footer() {
   return (
     <div className='footer'>
       <div className='socialMedia'>
-        <MdFacebook/>
-        <FaInstagram/>
         <FaLinkedinIn/>
+        <a href="mailto:juanjoseporras2019@gmail.com"><GrMail/></a>
         <FaGithub/>
         <FaYoutube/>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,84 @@
-import React from 'react'
-import { FaBootstrap, FaCss3, FaGit, FaHtml5, FaLinkedinIn, FaReact } from "react-icons/fa";
+import React from "react";
+import {
+  FaBootstrap,
+  FaCss3,
+  FaGit,
+  FaHtml5,
+  FaLinkedinIn,
+  FaReact,
+} from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
-import {SiTailwindcss, SiRedux, SiPhp, SiStyledcomponents, SiJavascript} from 'react-icons/si'
-import {DiMysql} from 'react-icons/di';
+import {
+  SiTailwindcss,
+  SiRedux,
+  SiPhp,
+  SiStyledcomponents,
+  SiJavascript,
+} from "react-icons/si";
+import { DiMysql } from "react-icons/di";
 
-import '../styles/Home.css'
-
+import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div className='home'>
-      <div className='about'>
+    <div className="home">
+      <div className="about">
         <h2>Hola, soy Juan José</h2>
-        <div className='prompt'>
+        <div className="prompt">
           <p>
-          "Transformar diseños en experiencias interactivas que cautivan y deleitan 
-          a los usuarios, se logra con una línea de código a la vez."
+            "Transformar diseños en experiencias interactivas que cautivan y
+            deleitan a los usuarios, se logra con una línea de código a la vez."
           </p>
-          <div className='icons'>
-            <FaLinkedinIn className='hvr-grow-shadow'/>
-            <FaGithub className='hvr-grow-shadow'/>
-            <FaYoutube className='hvr-grow-shadow'/>
+          <div className="icons">
+            <a
+              href="https://www.linkedin.com/in/juan-jos%C3%A9-porras-0b4301236/"
+              target="__blank"
+            >
+              <FaLinkedinIn className="hvr-grow-shadow" />
+            </a>
+            <a href="https://github.com/porras20" target="__blank">
+              <FaGithub className="hvr-grow-shadow" />
+            </a>
+            <a href="https://www.youtube.com/@jupoweb3906" target="__blank">
+              {" "}
+              <FaYoutube className="hvr-grow-shadow" />
+            </a>
           </div>
         </div>
       </div>
-      <div className='skills'>
+      <div className="skills">
         <h1>Skills</h1>
-        <ol className='list'>
-          <li className='item'>
+        <ol className="list">
+          <li className="item">
             <h2>Front-End</h2>
-            <div className='frontend_skills'>
-            <FaReact/> 
-            <FaBootstrap/>
-            <SiTailwindcss/>               
-            <FaHtml5/>
-            <FaCss3/>
-            <FaGit/>
-            <FaGithub/>
-            <SiRedux/>
-            <SiStyledcomponents/>
+            <div className="frontend_skills">
+              <FaReact />
+              <FaBootstrap />
+              <SiTailwindcss />
+              <FaHtml5 />
+              <FaCss3 />
+              <FaGit />
+              <FaGithub />
+              <SiRedux />
+              <SiStyledcomponents />
             </div>
           </li>
-          <li className='item'>
+          <li className="item">
             <h2>Back-End</h2>
-            <div className='frontend_skills'>
-            <DiMysql/>
+            <div className="frontend_skills">
+              <DiMysql />
             </div>
           </li>
-          <li className='item'>
+          <li className="item">
             <h2>Lenguajes</h2>
-            <div className='frontend_skills'>
-              <SiPhp/>
-              <SiJavascript/>
+            <div className="frontend_skills">
+              <SiPhp />
+              <SiJavascript />
             </div>
           </li>
         </ol>
       </div>
     </div>
-  )
+  );
 }
